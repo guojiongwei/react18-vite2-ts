a327b64fce97ccd34a7056b09fc7de7b664934c1
guojiongwei <18530633992@163.com>
2022-05-31 11:40:20 +0800

    feat: 配置eslint检测检测代码

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/jsx-runtime'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-non-null-assertion': 'off'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
--- src/main.tsx
+++ src/main.tsx
@@ -8,3 +8,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>
 )
+
+const a = 111
