5bc19e98b28e129b5017e2b417cadc535343794f
guojiongwei <18530633992@163.com>
2022-05-31 11:36:08 +0800

    feat: 配置prettier自动格式化代码

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,12 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button type='button' onClick={() => setCount(count => count + 1)}>
             count is: {count}
           </button>
         </p>
@@ -20,19 +20,19 @@ function App() {
         </p>
         <p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             Learn React
           </a>
           {' | '}
           <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='App-link'
+            href='https://vitejs.dev/guide/features.html'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             Vite Docs
           </a>
