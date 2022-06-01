d20db2b8faf04d99699a117051747ca3650167d8
guojiongwei <18530633992@163.com>
2022-06-01 14:25:58 +0800

    feat: 添加react-hooks语法eslint校验

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,8 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react/jsx-runtime'
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-
 function App() {
   const [count, setCount] = useState(0)
 
